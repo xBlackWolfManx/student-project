9a0cfea77e5b6413725f1067360a6da5fcb94e8d
Daniel Titov <bstb-201_173536@volsu.ru>
2023-05-20 20:25:59 +0300

    This is first version of this programm

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "std.h"
+
+int main()
+{
+    int a, b;
+
+    std::cout << "Enter first number " << endl;
+    std::cin >> a;
+
+    std::cout << "Enter second number " << endl;
+    std::cin >> b;
+}
