6470d2e9e0653c643dc592e29ca98c5d270d87fb
Daniel Titov <bstb-201_173536@volsu.ru>
2023-05-20 20:36:44 +0300

    Second version of this programme

---
+++ StudentProject/Studemt project/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    std::cout << "Enter first number " << endl;
+    std::cin >> a;
+
+    std::cout << "Enter second number " << endl;
+    std::cin >> b;
+    c = a+b;
+    cout << "Your answer is:" << endl;
+    cout << c << endl;
+}
